09bb5b36acefb000f7304c4ac4a48f0cd9698c60
Sean Griffin <seangriffin@internode.on.net>
2020-01-07 00:29:41 +1000

    Add files via upload

--- get_route_id.php
+++ get_route_id.php
@@ -5,6 +5,19 @@ $password = "";
 $dbname = "id12111839_bus";
 
 //echo $_POST['serviceShortName'];
+$services = json_decode($_POST["myServices"]);
+
+/*
+foreach($services as $service)
+{
+	echo $service->short_name;
+	echo $service->period;
+	echo $service->direction;
+}
+*/
+
+$myobj = new stdClass();
+$servicearr = array();
 
 
 
@@ -17,24 +30,70 @@ if ($conn->connect_error) {
 }
 //echo "Connected successfully";
 
-$sql = "SELECT routes.route_id FROM routes, main_trips WHERE routes.route_id = main_trips.route_id and routes.route_short_name = '" . $_POST['serviceShortName'] . "' and main_trips.direction = '" . $_POST['serviceDirection'] . "' and main_trips.trip_id like '%" . $_POST['servicePeriod'] . "%'";
-$result = $conn->query($sql);
 
-if (!$result) {
-    trigger_error('Invalid query: ' . $conn->error);
+
+foreach($services as $service)
+{
+	$serviceobj = new stdClass();
+
+
+//	$sql = "SELECT routes.route_id FROM routes, main_trips WHERE routes.route_id = main_trips.route_id and routes.route_short_name = '" . $_POST['serviceShortName'] . "' and main_trips.direction = '" . $_POST['serviceDirection'] . "' and main_trips.trip_id like '%" . $_POST['servicePeriod'] . "%'";
+	$sql = "SELECT routes.route_id FROM routes, main_trips WHERE routes.route_id = main_trips.route_id and routes.route_short_name = '" . $service->short_name . "' and main_trips.direction = '" . $service->direction . "' and main_trips.trip_id like '%" . $service->period . "%'";
+	$result = $conn->query($sql);
+
+	if (!$result) {
+		trigger_error('Invalid query: ' . $conn->error);
+	}
+
+
+
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+	//        echo 'routeid' => $row["route_id"];
+	//        echo trim($row["route_id"]);
+			
+	//		array_push($newarr, 'main_id' => $row["route_id"]);
+			$serviceobj->main_id = $row["route_id"];
+			
+
+			$sql = "SELECT route_id FROM routes WHERE route_short_name = '" . $service->short_name . "'";
+			$result = $conn->query($sql);
+
+			if (!$result) {
+				trigger_error('Invalid query: ' . $conn->error);
+			}
+
+
+			if ($result->num_rows > 0) {
+				
+				$newarr = array();
+				
+				// output data of each row
+				while($row = $result->fetch_assoc()) {
+			
+	//				$tmparr = array();
+		//			$tmparr = ['id' => $row["route_id"]];
+			//		array_push($newarr, $tmparr);
+					array_push($newarr, $row["route_id"]);
+				}
+				
+				$serviceobj->all_id = $newarr;
+
+			}
+			
+		}
+	}
+	
+	array_push($servicearr, $serviceobj);
+
 }
 
-$newarr = array();
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-//        echo 'routeid' => $row["route_id"];
-        echo trim($row["route_id"]);
-    }
-} else {
-    echo "0 results";
-}
 $conn->close();
+
+$myobj->services = $servicearr;
+//echo json_encode($newarr);
+echo json_encode($myobj);
+
 
 ?> 
--- get_stops.php
+++ get_stops.php
@@ -4,6 +4,8 @@ $username = "id12111839_sean";
 $password = "";
 $dbname = "id12111839_bus";
 
+$services = json_decode($_POST["myServices"]);
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -13,30 +15,79 @@ if ($conn->connect_error) {
 }
 //echo "Connected successfully";
 
-$sql = "SELECT stops.stop_id, main_stop_times.stop_sequence, stops.stop_lat, stops.stop_lon FROM routes, main_trips, main_stop_times, stops WHERE routes.route_id = main_trips.route_id and main_trips.trip_id = main_stop_times.trip_id and main_stop_times.stop_id = stops.stop_id and routes.route_short_name = '" . $_POST['serviceShortName'] . "' and main_trips.trip_id like '%" . $_POST['servicePeriod'] . "%' and main_trips.direction = '" . $_POST['serviceDirection'] . "'";
-//$sql = "SELECT stops.stop_id, stops.stop_lat, stops.stop_lon FROM routes, main_trips, main_stop_times, stops WHERE routes.route_id = main_trips.route_id and main_trips.trip_id = main_stop_times.trip_id and main_stop_times.stop_id = stops.stop_id and routes.route_short_name = '130' and main_trips.trip_id like '%Weekday%' and main_trips.direction = 0";
-//$sql = "SELECT stop_id FROM stops";
-$result = $conn->query($sql);
+$servicesobj = new stdClass();
+$servicesarr = array();
 
-if (!$result) {
-    trigger_error('Invalid query: ' . $conn->error);
-}
 
-$newarr = array();
+foreach($services as $service)
+{
+	$servicestopsobj = new stdClass();
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-     //   echo "id: " . $row["stop_id"]. " - Name: " . $row["stop_lat"]. " " . $row["stop_lon"]. "<br>";
-		
-		$tmparr = ['id' => $row["stop_id"], 'sequence' => $row["stop_sequence"], 'lat' => $row["stop_lat"], 'lng' => $row["stop_lon"]];
-		array_push($newarr, $tmparr);
-    }
-} else {
-    echo "0 results";
+
+	$sql = "SELECT stops.stop_id, main_stop_times.stop_sequence, stops.stop_lat, stops.stop_lon FROM routes, main_trips, main_stop_times, stops WHERE routes.route_id = main_trips.route_id and main_trips.trip_id = main_stop_times.trip_id and main_stop_times.stop_id = stops.stop_id and routes.route_short_name = '" . $service->short_name . "' and main_trips.trip_id like '%" . $service->period . "%' and main_trips.direction = '" . $service->direction . "' order by main_stop_times.stop_sequence";
+	$result = $conn->query($sql);
+
+	if (!$result) {
+		trigger_error('Invalid query: ' . $conn->error);
+	}
+
+	$newarr = array();
+	$prev_stop_lat = "";
+	$prev_stop_lng = "";
+	$distance = 0;
+
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			
+			if ($prev_stop_lat !== "")
+			
+				$distance = (int)haversineGreatCircleDistance($row["stop_lat"], $row["stop_lon"], $prev_stop_lat, $prev_stop_lng);
+			
+			$tmparr = ['id' => $row["stop_id"], 'sequence' => $row["stop_sequence"], 'lat' => $row["stop_lat"], 'lng' => $row["stop_lon"], 'distance_to_previous' => $distance];
+			array_push($newarr, $tmparr);
+			
+			$prev_stop_lat = $row["stop_lat"];
+			$prev_stop_lng = $row["stop_lon"];
+		}
+	}
+	
+	$servicestopsobj->stops = $newarr;
+	array_push($servicesarr, $servicestopsobj);
+
 }
 $conn->close();
 
-echo json_encode($newarr);
+$servicesobj->services = $servicesarr;
+echo json_encode($servicesobj);
+
+
+/**
+ * Calculates the great-circle distance between two points, with
+ * the Haversine formula.
+ * @param float $latitudeFrom Latitude of start point in [deg decimal]
+ * @param float $longitudeFrom Longitude of start point in [deg decimal]
+ * @param float $latitudeTo Latitude of target point in [deg decimal]
+ * @param float $longitudeTo Longitude of target point in [deg decimal]
+ * @param float $earthRadius Mean earth radius in [m]
+ * @return float Distance between points in [m] (same as earthRadius)
+ */
+function haversineGreatCircleDistance(
+  $latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371000)
+{
+  // convert from degrees to radians
+  $latFrom = deg2rad($latitudeFrom);
+  $lonFrom = deg2rad($longitudeFrom);
+  $latTo = deg2rad($latitudeTo);
+  $lonTo = deg2rad($longitudeTo);
+
+  $latDelta = $latTo - $latFrom;
+  $lonDelta = $lonTo - $lonFrom;
+
+  $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
+    cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+  return $angle * $earthRadius;
+}
+
 
 ?> 
